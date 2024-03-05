3e2c99f38ea45a1b7716a284e31681bf05458905
Dusk <65917000+duskdev17@users.noreply.github.com>
2024-03-05 22:36:10 +0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+} from "react-native";
+import Task from "./components/Task";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function App() {
+  const [task, setTask] = useState("");
+  const [taskItems, setTaskItems] = useState([]);
+
+  // Load tasks from AsyncStorage on component mount
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  // Save tasks to AsyncStorage whenever taskItems change
+  useEffect(() => {
+    saveTasks();
+  }, [taskItems]);
+
+  const handleTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask("");
+  };
+
+  const completeTask = (index) => {
+    let itemCopy = [...taskItems];
+    itemCopy.splice(index, 1);
+    setTaskItems(itemCopy);
+  };
+
+  const saveTasks = async () => {
+    try {
+      await AsyncStorage.setItem("taskItems", JSON.stringify(taskItems));
+    } catch (error) {
+      console.error("Error saving tasks to AsyncStorage:", error);
+    }
+  };
+
+  const loadTasks = async () => {
+    try {
+      const storedTasks = await AsyncStorage.getItem("taskItems");
+      if (storedTasks !== null) {
+        setTaskItems(JSON.parse(storedTasks));
+      }
+    } catch (error) {
+      console.error("Error loading tasks from AsyncStorage:", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <ScrollView style={styles.items}>
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          onChangeText={(text) => setTask(text)}
+          value={task}
+        />
+
+        <TouchableOpacity onPress={() => handleTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FDF7E4",
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    borderColor: "#BBAB8C",
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#BBAB8C",
+    borderWidth: 1,
+  },
+  addText: {},
+});
---
+++ components/Task.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#DED0B6",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#BBAB8C",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderWidth: 2,
+    borderColor: "#BBAB8C",
+    borderRadius: 5,
+  },
+});
+
+export default Task;
